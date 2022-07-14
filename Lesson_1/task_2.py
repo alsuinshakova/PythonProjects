93f6a88f0b5592b90c9ed5ea66e19ccf713fef0c
Алсу <108096197+alsuinshakova@users.noreply.github.com>
2022-07-14 21:59:46 +0300

    Tasks 1-61

--- Lesson_1/task_2.py
+++ Lesson_1/task_2.py
@@ -1,5 +1,5 @@
-time = int(input("Enter the time in seconds:"))
-hours = time // 3600
-minutes = (time % 3600) // 60
-seconds = (time % 3600) % 60
+time1 = int(input("Enter the time in seconds:"))
+hours = time1 // 3600
+minutes = (time1 % 3600) // 60
+seconds = (time1 % 3600) % 60
 print(hours, "hours", minutes, "minutes", seconds, "seconds")
--- Lesson_1/task_6.py
+++ Lesson_1/task_6.py
@@ -3,7 +3,7 @@ while True:
     start_km = float(input("Стартовый результат:"))
     last_km = float(input("Финальный результат:"))
     if start_km <= 0 or last_km < 0:
-        print("Результаты должны быть больше нуля. Стартовое значение!= 0.")
+        print("Результаты должны быть больше нуля, стартовое значение!= 0.")
     else:
         while start_km < last_km:
             start_km += start_km * 0.1
