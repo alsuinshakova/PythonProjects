733e32515c12cfac3167e9dfab74fff397b36558
Алсу <108096197+alsuinshakova@users.noreply.github.com>
2022-07-14 20:26:40 +0300

    Tasks 1-6

---
+++ Lesson_1/task_1.py
@@ -0,0 +1,8 @@
+name = input("Enter your name:")
+print("Hi", name)
+a = int(input("Enter number"))
+b = int(input("Enter number"))
+c = int(a + b)
+print(c)
+
+
---
+++ Lesson_1/task_6.py
@@ -0,0 +1,12 @@
+while True:
+    days = 1
+    start_km = float(input("Стартовый результат:"))
+    last_km = float(input("Финальный результат:"))
+    if start_km <= 0 or last_km < 0:
+        print("Результаты должны быть больше нуля. Стартовое значение!= 0.")
+    else:
+        while start_km < last_km:
+            start_km += start_km * 0.1
+            days += 1
+        print(f"Спортсмен добьется результата за {days} дней")
+        break
