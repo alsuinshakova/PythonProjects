2a7f4e94c74af0a438e51e437ba6626525ba2fbb
Алсу <108096197+alsuinshakova@users.noreply.github.com>
2022-07-14 22:12:36 +0300

    Tasks 1-61

---
+++ Lesson_2/task_3.py
@@ -0,0 +1,7 @@
+seasons_dict = {'Зима': [12, 1, 2], 'Весна': [3, 4, 5], 'Лето': [6, 7, 8], 'Осень': [9, 10, 11]}
+month_num = int(input('Введи номер месяца(только цифра): '))
+if month_num in sum(seasons_dict.values(), []):
+    for i in seasons_dict.items():
+        if month_num in i[1]:
+            print(i[0])
+            break
---
+++ Lesson_2/task_5.py
@@ -0,0 +1,13 @@
+my_list = [5, 4, 4, 3, 2, 1]
+new_number = ""
+while new_number != "q":
+    i = 0
+    new_number = input("Enter a new rating element in the form of a natural number.\nTo exit - q\n ")
+    if new_number.isdigit():
+        for n in my_list:
+            if int(new_number) <= n:
+                i += 1
+            else:
+                break
+        my_list.insert(i, int(new_number))
+    print(my_list)
