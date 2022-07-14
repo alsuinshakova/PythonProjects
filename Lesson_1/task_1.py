e859bcc1ad6cb1a81cef430641840ed392225b89
Алсу <108096197+alsuinshakova@users.noreply.github.com>
2022-07-14 20:25:01 +0300

    delete task 1-6

--- Lesson_1/task_1.py
+++
@@ -1,8 +0,0 @@
-name = input("Enter your name:")
-print("Hi", name)
-a = int(input("Enter number"))
-b = int(input("Enter number"))
-c = int(a + b)
-print(c)
-
-
--- Lesson_1/task_3.py
+++
@@ -1,2 +0,0 @@
-num = input("Введите цифру: ")
-print(f"{num} + {num + num} + {num + num + num} = {int(num) + int(num + num) + int(num + num + num)}")
